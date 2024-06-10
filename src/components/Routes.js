bd92f22bc0ca013970296585ae74248302a78bcf
Vedat <mvedataydin@gmail.com>
2024-06-10 14:43:59 +0300

    Create src/components/Routes.js

---
+++ src/components/Routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+const Routes = () => (
+  <Switch>
+    {/* Define your route components here */}
+  </Switch>
+);
+
+export default Routes;
