fc7e5a2dc5b1831f7b2bca138b47564d1d9e6962
Mehmet Vedat Aydin <mvedataydin@gmail.com>
2024-06-18 00:57:17 +0300

    feat: update career page

--- src/pages/Career.tsx
+++ src/pages/Career.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import CardList from '../components/CardList';
 
 const Career: React.FC = () => {
-  return <div>Career Page</div>;
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-8">My Projects</h1>
+      <CardList />
+    </div>
+  );
 };
 
 export default Career;
