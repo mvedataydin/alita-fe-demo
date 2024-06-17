874ecfc311c7b730e4c4898866438f8354671033
Vedat <mvedataydin@gmail.com>
2024-06-17 23:50:16 +0300

    Update src/pages/Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,32 @@
 import { Button } from 'antd';
+import { Button } from 'antd';
 import { useState, useEffect } from 'react';
+const MOCK_HTML_STRING = '<p>This is some <strong>HTML content</strong>.</p>';
 
-const Home: React.FC = () => {
-  return <div>Home Page</div>;
+const HomePage = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const handleClick = () => {
+    setCounter(0);
+  };
+
+  return (
+    <div>
+      <p>This is the home page.</p>
+
+      <Button onClick={handleClick}>Reset Count</Button>
+      <p>Count: {counter}</p>
+      {/* It's important to sanitize any HTML content before using dangerouslySetInnerHTML to prevent XSS attacks. */}
+      <div dangerouslySetInnerHTML={{ __html: MOCK_HTML_STRING }} />
+    </div>
+  );
 };
 
-export default Home;
+export default HomePage;
